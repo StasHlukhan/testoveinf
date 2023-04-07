2466aa78bf751c4324e4a42b5f350a963f79b41a
stasik132 <stas1705200563@gmail.com>
2023-04-07 17:52:17 +0300

    add anim

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react'
 import { IProduct } from '../../types/types';
 import ProductsItem from './ProductsItem';
-
+import '../../App.css'
+import { TransitionGroup,CSSTransition } from 'react-transition-group'
 
 
 interface ProductsListProps {
@@ -11,9 +12,16 @@ interface ProductsListProps {
 const ProductsList: FC<ProductsListProps> = ({products,removeProduct}) => {
     return (
         <div>
+             <TransitionGroup>
             {products.map((product) =>
+             <CSSTransition key={product.id}
+             timeout={500}
+             classNames="post">
                 <ProductsItem  removeProduct={removeProduct} key={product.id} product={product}  ></ProductsItem>
+             </CSSTransition>
+                
             )}
+            </TransitionGroup>
         </div>
     )
 }
